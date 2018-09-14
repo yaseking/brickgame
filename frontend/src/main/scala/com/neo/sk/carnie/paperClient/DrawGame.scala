641067433b91891ef7d25050f27f6e9330b029ba
anjiansan <huangzhiwei@neootel.com.cn>
2018-09-14 15:22:55 +0800

    rank scale

--- frontend/src/main/scala/com/neo/sk/carnie/paperClient/DrawGame.scala
+++ frontend/src/main/scala/com/neo/sk/carnie/paperClient/DrawGame.scala
@@ -238,13 +238,13 @@ class DrawGame(
 
 //    //边界离屏
     ctx.drawImage(borderCanvas, offx * canvasUnit, offy * canvasUnit)
-//    ctx.restore()
+    ctx.restore()
 //
 //    //排行榜边界离屏
     rankCtx.clearRect(20, textLineHeight * 5, 600, textLineHeight * 2)
     PerformanceTool.renderFps(rankCtx, 20, 5 * textLineHeight)
     ctx.drawImage(rankCanvas, 0, 0)
-    ctx.restore()
+//    ctx.restore()
 
   }
 
