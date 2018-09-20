7c30705dd9bd8da6ae08e8dbeb34f8312839a099
litianyu <87394376@qq.com>
2018-09-20 11:49:53 +0800

    test version

--- backend/src/main/scala/com/neo/sk/carnie/paperClient/PlayGround.scala
+++ backend/src/main/scala/com/neo/sk/carnie/paperClient/PlayGround.scala
@@ -162,7 +162,7 @@ object PlayGround {
                 userInRoom.filterNot(finishUsers.contains(_)).foreach(u => dispatchTo(u, NewFieldInfo(grid.frameCount, newField)))
               }
               if(tickCount % 10 == 3) dispatch(Protocol.Ranks(r._2._2.currentRank, r._2._2.historyRankList), r._1)
-              if (r._2._2.currentRank.nonEmpty && r._2._2.currentRank.head.area >= winStandard * (0.7 - r._2._1*0.05)) {
+              if (r._2._2.currentRank.nonEmpty && r._2._2.currentRank.head.area >= winStandard * (0.3 - r._2._1*0.05)) {
                 r._2._2.cleanData()
                 dispatch(Protocol.SomeOneWin(userMap(r._2._2.currentRank.head.id)._2), r._1)
               }
