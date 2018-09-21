0fda48dd671bb067b90caf3b34d624f6b61ce5f0
litianyu <87394376@qq.com>
2018-09-21 17:00:43 +0800

    debug winStandard

--- backend/src/main/scala/com/neo/sk/carnie/paperClient/PlayGround.scala
+++ backend/src/main/scala/com/neo/sk/carnie/paperClient/PlayGround.scala
@@ -40,7 +40,8 @@ object PlayGround {
 
   private val limitNum = 8
 
-  private val winStandard = (BorderSize.w - 2) * (BorderSize.h - 2)
+  private val fullSize = (BorderSize.w - 2) * (BorderSize.h - 2)
+  private var winStandard = (BorderSize.w - 2) * (BorderSize.h - 2) * 0.7
 
   def create(system: ActorSystem)(implicit executor: ExecutionContext): PlayGround = {
 
@@ -162,7 +163,10 @@ object PlayGround {
                 userInRoom.filterNot(finishUsers.contains(_)).foreach(u => dispatchTo(u, NewFieldInfo(grid.frameCount, newField)))
               }
               if(tickCount % 10 == 3) dispatch(Protocol.Ranks(r._2._2.currentRank), r._1)
-              if (r._2._2.currentRank.nonEmpty && r._2._2.currentRank.head.area >= winStandard * (0.2 - r._2._1*0.05)) {
+              val maxSize = r._2._2.currentRank.head.area
+              if((maxSize+fullSize*0.1)<winStandard)
+                winStandard = fullSize * (0.7 - r._2._1*0.05)
+              if (r._2._2.currentRank.nonEmpty && r._2._2.currentRank.head.area >= winStandard) {
                 r._2._2.cleanData()
                 dispatch(Protocol.SomeOneWin(userMap(r._2._2.currentRank.head.id)._2), r._1)
               }
