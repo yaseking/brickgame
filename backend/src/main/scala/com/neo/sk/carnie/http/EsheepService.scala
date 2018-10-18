459eb096d033f821570d61b735899a731433bc18
litianyu <87394376@qq.com>
2018-10-18 17:36:58 +0800

    连接游戏（包含鉴权） untested

---
+++ backend/src/main/scala/com/neo/sk/carnie/http/EsheepService.scala
@@ -0,0 +1,54 @@
+package com.neo.sk.carnie.http
+
+import com.neo.sk.carnie.utils.CirceSupport
+import org.slf4j.LoggerFactory
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import com.neo.sk.carnie.common._
+import com.neo.sk.carnie.utils.EsheepClient
+import com.neo.sk.carnie.Boot.executor
+
+trait EsheepService extends ServiceUtils with CirceSupport with PlayerService{
+
+  private val log = LoggerFactory.getLogger(this.getClass)
+
+  private val expireTime = 10*60*1000l
+
+  private val playGame = (path("playGame") & get & pathEndOrSingleSlash) {
+    parameter(
+      'playerId.as[Long],
+      'playerName.as[String],
+      'roomId.as[Int].?,
+      'accessCode.as[String],
+      'appId.as[String],
+      'secureKey.as[String],
+    ) {
+      case (playerId, playerName, roomId, accessCode, appId, secureKey) =>
+        if(AppSettings.appSecureMap.contains(appId) && (AppSettings.appSecureMap(appId) == secureKey)){
+          val gameId = AppSettings.esheepGameId
+          dealFutureResult{
+            EsheepClient.verifyAccessCode(gameId, accessCode).map {
+              case Right(rsp) =>
+                if(rsp.playerId == playerId && rsp.nickName == playerName){
+                  //join Game
+                  webSocketChatFlow(playerName)
+                  complete()
+                } else {
+                  complete()
+                }
+              case Left(e) =>
+                log.error(s"playGame error. fail to verifyAccessCode err: $e")
+                complete()
+            }
+          }
+        } else {
+          complete()
+        }
+    }
+  }
+
+  val esheepRoute: Route = pathPrefix("game") {
+    playGame
+  }
+
+}
