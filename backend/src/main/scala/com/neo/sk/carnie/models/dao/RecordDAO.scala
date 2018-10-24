48c72fff40bba9f7d36d0d0384a26deb8dd4a1ae
Jason <qiulinhui@neotel.com.cn>
2018-10-24 11:32:14 +0800

    获取比赛录像列表接口

--- backend/src/main/scala/com/neo/sk/carnie/models/dao/RecordDAO.scala
+++ backend/src/main/scala/com/neo/sk/carnie/models/dao/RecordDAO.scala
@@ -1,24 +1,75 @@
 package com.neo.sk.carnie.models.dao
 
-import scala.concurrent.Future
-import com.neo.sk.carnie.models.SlickTables._
 import com.neo.sk.utils.DBUtil.db
 import slick.jdbc.PostgresProfile.api._
+import com.neo.sk.carnie.models.SlickTables._
 import com.neo.sk.carnie.Boot.executor
+import com.neo.sk.carnie.models.SlickTables
+import net.sf.ehcache.search.aggregator.Count
+
+import scala.concurrent.Future
 
 /**
-  * Created by dry on 2018/10/23.
-  **/
+  * User: Jason
+  * Date: 2018/10/22
+  * Time: 14:49
+  */
 object RecordDAO {
-
-  def saveGameRecorder(roomId: Int, startTime: Long, endTime: Long, filePath: String): Future[Long] = {
-    db.run{
-      tGameRecord.returning(tGameRecord.map(_.recordId)) += rGameRecord(-1l, roomId, startTime, endTime, filePath)
+  def getRecordList(lastRecord: Long,count: Int)= {
+    if(lastRecord == 0L){
+      val action = {
+        tGameRecord.sortBy(_.recordId.desc).take(count) join tUserInRecord on { (game, user) =>
+          game.recordId === user.recordId
+        }
+      }.result
+      db.run(action)
+    }
+    else{
+      val action = {
+        tGameRecord.filter(_.recordId < lastRecord).sortBy(_.recordId.desc).take(count) join tUserInRecord on { (game, user) =>
+          game.recordId === user.recordId
+        }
+      }.result
+      db.run(action)
     }
   }
 
-  def saveUserInGame(users: Set[rUserInRecord]) = {
-    db.run(tUserInRecord ++= users)
+  def getRecordListByTime(startTime: Long,endTime: Long,lastRecord: Long,count: Int) = {
+    if(lastRecord == 0L){
+      val action = {
+        tGameRecord.sortBy(_.recordId.desc).take(count) join tUserInRecord on { (game, user) =>
+          (game.recordId === user.recordId) && game.startTime >= startTime && game.endTime <= endTime
+        }
+      }.result
+      db.run(action)
+    }
+    else{
+      val action = {
+        tGameRecord.filter(_.recordId < lastRecord).sortBy(_.recordId.desc).take(count) join tUserInRecord on { (game, user) =>
+          game.recordId === user.recordId && game.startTime >= startTime && game.endTime <= endTime
+        }
+      }.result
+      db.run(action)
+    }
+
   }
 
+  def getRecordListByPlayer(playerId: String,lastRecord: Long,count: Int) = {
+    if(lastRecord == 0L){
+      val action = {
+        tGameRecord.sortBy(_.recordId.desc).take(count) join tUserInRecord on { (game, user) =>
+          (game.recordId === user.recordId) && user.userId === playerId
+        }
+      }.result
+      db.run(action)
+    }
+    else{
+      val action = {
+        tGameRecord.filter(_.recordId < lastRecord).sortBy(_.recordId.desc).take(count) join tUserInRecord on { (game, user) =>
+          (game.recordId === user.recordId) && user.userId === playerId
+        }
+      }.result
+      db.run(action)
+    }
+  }
 }
