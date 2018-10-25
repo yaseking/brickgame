e6bf8287761e691affd960cf9c951708eba7f6be
litianyu <87394376@qq.com>
2018-10-25 11:34:05 +0800

    save

--- client/src/main/scala/com/neo/sk/carnie/paperClient/Constant.scala
+++ client/src/main/scala/com/neo/sk/carnie/paperClient/Constant.scala
@@ -17,16 +17,16 @@ object Constant {
 
   object ColorsSetting {
     val backgroundColor = "#F5F5F5"
-    val fontColor2 = "#000000"
-    val gameNameColor = "#5BC48C"
-    val fontColor = "#E0EEFD"
+    val fontColor2 = "#000000"//(0,0,0)
+    val gameNameColor = "#5BC48C"//(91,196,140)
+    val fontColor = "#E0EEFD"//(224,238,253)
     val defaultColor = "#000080"
-    val borderColor = "#696969"
-    val mapColor = "#C0C0C0"
+    val borderColor = "#696969"//(105,105,105)
+    val mapColor = "#C0C0C0"//192,192,192
     val redColor = "#FF0000"
     val greenColor = "#00FF00"
     val yellowColor = "#FFFF00"
-    val backgroundColor2 = "#333333"
+    val backgroundColor2 = "#333333"//(51,51,51)
   }
 
 
---
+++ client/src/main/scala/com/neo/sk/carnie/paperClient/PerformanceTool.scala
@@ -0,0 +1,68 @@
+package com.neo.sk.carnie.paperClient
+
+import com.neo.sk.carnie.paperClient.Constant.ColorsSetting
+import com.neo.sk.carnie.paperClient.Protocol.ReceivePingPacket
+import javafx.scene.canvas.GraphicsContext
+import javafx.scene.paint.Color
+
+/**
+  * Created by dry on 2018/9/3.
+  **/
+object PerformanceTool {
+  //FPS
+  private var lastRenderTime = System.currentTimeMillis()
+  private var lastRenderTimes = 0
+  private var renderTimes = 0
+  private var tempTime = System.currentTimeMillis()
+
+  private def addFps() = {
+    val time = System.currentTimeMillis()
+    renderTimes += 1
+//    println(s"addFps time:${time - tempTime}")
+    tempTime = time
+    if (time - lastRenderTime > 1000) {
+      lastRenderTime = time
+      lastRenderTimes = renderTimes
+      renderTimes = 0
+    }
+  }
+
+  def renderFps(ctx: GraphicsContext, leftBegin: Int, lineHeight: Int) = {
+    addFps()
+//    ctx.font = "14px Helvetica"
+    ctx.textAlign = "start"
+//    val fpsString = s"fps : $lastRenderTimes, ping: $latency"
+//    ctx.fillText(fpsString, leftBegin, lineHeight)
+
+    ctx.font = "20px Helvetica"
+    ctx.fillStyle = ColorsSetting.fontColor2
+    val fpsString = "fps : "
+    val pingString = "ping: "
+    ctx.fillText(fpsString, leftBegin, lineHeight)
+    ctx.fillText(pingString, leftBegin + ctx.measureText(fpsString).width + 50, lineHeight)
+    ctx.setStroke(Color.BLACK)
+//    ctx.strokeStyle = "black"
+    ctx.strokeText(lastRenderTimes.toString, leftBegin + ctx.measureText(fpsString).width, lineHeight)
+    ctx.fillStyle = if (lastRenderTimes < 50) ColorsSetting.redColor else ColorsSetting.greenColor
+    ctx.fillText(lastRenderTimes.toString, leftBegin + ctx.measureText(fpsString).width, lineHeight)
+    ctx.strokeStyle = "black"
+    ctx.strokeText(s"${latency}ms", leftBegin + ctx.measureText(fpsString).width + ctx.measureText(pingString).width + 60, lineHeight)
+    ctx.fillStyle = if (latency <= 100) ColorsSetting.greenColor else if (latency > 100 && latency <= 200) ColorsSetting.yellowColor else ColorsSetting.redColor
+    ctx.fillText(s"${latency}ms", leftBegin + ctx.measureText(fpsString).width + ctx.measureText(pingString).width + 60, lineHeight)
+
+  }
+
+  //PING
+  private var receiveNetworkLatencyList: List[Long] = Nil
+  private val PingTimes = 10
+  private var latency: Long = 0L
+
+  def receivePingPackage(p: ReceivePingPacket): Unit = {
+    receiveNetworkLatencyList = (System.currentTimeMillis() - p.createTime) :: receiveNetworkLatencyList
+    if (receiveNetworkLatencyList.size >= PingTimes) {
+      latency = receiveNetworkLatencyList.sum / receiveNetworkLatencyList.size
+      receiveNetworkLatencyList = Nil
+    }
+  }
+
+}
