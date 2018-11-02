c3683feee12b15903f10b59b818f4cecb4ead8fa
litianyu <87394376@qq.com>
2018-11-02 10:23:53 +0800

    fix

--- client/src/main/scala/com/neo/sk/carnie/actor/PlayGameWebSocket.scala
+++ client/src/main/scala/com/neo/sk/carnie/actor/PlayGameWebSocket.scala
@@ -126,7 +126,7 @@ object PlayGameWebSocket {
 
   def getWebSocketUri(playerId: String, playerName: String, accessCode: String, domain: String): String = {
     val wsProtocol = "ws"
-//    val domain = "10.1.29.250:30368"
+    val domain = "10.1.29.250:30368"
     s"$wsProtocol://$domain/carnie/join?id=$playerId&name=$playerName"
   }
 
