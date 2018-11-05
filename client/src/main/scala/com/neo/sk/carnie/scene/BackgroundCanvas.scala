8a7662c9e9568379382cda85725a650ba45cfb72
litianyu <87394376@qq.com>
2018-11-05 23:42:26 +0800

    ...

--- client/src/main/scala/com/neo/sk/carnie/scene/BackgroundCanvas.scala
+++ client/src/main/scala/com/neo/sk/carnie/scene/BackgroundCanvas.scala
@@ -12,6 +12,8 @@ class BackgroundCanvas(canvas: Canvas) {
   private val ctx = canvas.getGraphicsContext2D
   private val canvasUnit = 20
 
+  def getBackgroundCanvas = canvas
+
   def drawCache(offx: Float, offy: Float): Unit = { //离屏缓存的更新--缓存边界
     ctx.clearRect(0,0,canvas.getWidth,canvas.getHeight)
     ctx.setFill(Color.rgb(105,105,105))
