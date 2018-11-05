7d782888a79b6c1c4a39af6fbb960a7c8bece1ce
dongruoyan <dongruoyan@neotel.com.cn>
2018-11-05 15:17:07 +0800

    debug

--- backend/src/main/scala/com/neo/sk/carnie/http/RoomApiService.scala
+++ backend/src/main/scala/com/neo/sk/carnie/http/RoomApiService.scala
@@ -59,7 +59,7 @@ trait RoomApiService extends ServiceUtils with CirceSupport with PlayerService w
 
 
   private val getRoomList = (path("getRoomList") & post & pathEndOrSingleSlash) {
-    dealPostReq[AllRoomReq] { req =>
+    dealPostReqWithoutData {
       val msg: Future[List[Int]] = roomManager ? RoomManager.FindAllRoom
       msg.map {
         allRoom =>
