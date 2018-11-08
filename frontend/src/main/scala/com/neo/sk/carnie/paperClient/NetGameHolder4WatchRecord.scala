74381b871f443bacaa123ce54eaddcf36d10c1e3
pro <haoshuhan@neotel.com.cn>
2018-11-08 20:54:18 +0800

    .

--- frontend/src/main/scala/com/neo/sk/carnie/paperClient/NetGameHolder4WatchRecord.scala
+++ frontend/src/main/scala/com/neo/sk/carnie/paperClient/NetGameHolder4WatchRecord.scala
@@ -269,9 +269,9 @@ class NetGameHolder4WatchRecord(webSocketPara: WatchRecordPara){
         replayFinish = true
 
       case Protocol.ReplayFrameData(frameIndex, eventsData, stateData) =>
-        println(s"receive replayFrameData")
-        println(s"grid.frameCount:${grid.frameCount}")
-        println(s"frameIndex:$frameIndex")
+//        println(s"receive replayFrameData")
+//        println(s"grid.frameCount:${grid.frameCount}")
+//        println(s"frameIndex:$frameIndex")
         if(frameIndex == 0) grid.frameCount = 0
         if(stateData.nonEmpty) {
           stateData.get match {
