951a107bf0f04158aa5caeee6f697884231b3513
pro <haoshuhan@neotel.com.cn>
2018-11-09 11:00:13 +0800

    .

--- frontend/src/main/scala/com/neo/sk/carnie/paperClient/NetGameHolder4WatchRecord.scala
+++ frontend/src/main/scala/com/neo/sk/carnie/paperClient/NetGameHolder4WatchRecord.scala
@@ -144,7 +144,7 @@ class NetGameHolder4WatchRecord(webSocketPara: WatchRecordPara){
           dom.window.cancelAnimationFrame(nextFrame)
           isContinue = false
         } else {
-//          println(s"data.snakes:::${data.snakes}")
+          println(s"firstCome:::$firstCome")
           data.snakes.find(_.id == myId) match {
             case Some(snake) =>
               firstCome = false
