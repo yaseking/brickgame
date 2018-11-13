793d057551c43a64254a0b546d8deaf98f9929c4
Jason <qiulinhui@neotel.com.cn>
2018-11-13 15:38:03 +0800

    debug

--- backend/src/main/scala/com/neo/sk/carnie/http/RoomApiService.scala
+++ backend/src/main/scala/com/neo/sk/carnie/http/RoomApiService.scala
@@ -79,7 +79,7 @@ trait RoomApiService extends ServiceUtils with CirceSupport with PlayerService w
   private val getRecordList = (path("getRecordList") & post & pathEndOrSingleSlash) {
     dealPostReq[RecordListReq] { req =>
       RecordDAO.getRecordList(req.lastRecordId, req.count).map { recordL =>
-        complete(RecordListRsp(recordL.toList.map(_._1).distinct.sortWith((a, b) => a.recordId > b.recordId).reverse.map { r =>
+        complete(RecordListRsp(recordL.toList.map(_._1).distinct.sortWith((a, b) => a.recordId > b.recordId).map { r =>
           val userList = recordL.map(i => i._2).distinct.filter(_.recordId == r.recordId).map(_.userId)
           recordInfo(r.recordId, r.roomId, r.startTime, r.endTime, userList.length, userList)
         }))
@@ -90,7 +90,7 @@ trait RoomApiService extends ServiceUtils with CirceSupport with PlayerService w
   private val getRecordListByTime = (path("getRecordListByTime") & post & pathEndOrSingleSlash) {
     dealPostReq[RecordByTimeReq] { req =>
       RecordDAO.getRecordListByTime(req.startTime, req.endTime, req.lastRecordId, req.count).map { recordL =>
-        complete(RecordListRsp(recordL.toList.map(_._1).distinct.sortWith((a, b) => a.recordId > b.recordId).take(req.count).reverse.map { r =>
+        complete(RecordListRsp(recordL.toList.map(_._1).distinct.sortWith((a, b) => a.recordId > b.recordId).take(req.count).map { r =>
           val userList = recordL.map(i => i._2).distinct.filter(_.recordId == r.recordId).map(_.userId)
           recordInfo(r.recordId, r.roomId, r.startTime, r.endTime, userList.length, userList)
         }))
@@ -101,7 +101,7 @@ trait RoomApiService extends ServiceUtils with CirceSupport with PlayerService w
   private val getRecordListByPlayer = (path("getRecordListByPlayer") & post & pathEndOrSingleSlash) {
     dealPostReq[RecordByPlayerReq] { req =>
       RecordDAO.getRecordListByPlayer(req.playerId, req.lastRecordId, req.count).map { recordL =>
-        complete(RecordListRsp(recordL.toList.filter(_._2.userId == req.playerId).map(_._1).distinct.sortWith((a, b) => a.recordId > b.recordId).take(req.count).reverse.map { r =>
+        complete(RecordListRsp(recordL.toList.filter(_._2.userId == req.playerId).map(_._1).distinct.sortWith((a, b) => a.recordId > b.recordId).take(req.count).map { r =>
           val userList = recordL.map(i => i._2).distinct.filter(_.recordId == r.recordId).map(_.userId)
           recordInfo(r.recordId, r.roomId, r.startTime, r.endTime, userList.length, userList)
         }))
