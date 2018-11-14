3d1090054e5ba7c6f3f831487efeb7f1ea87a0ec
dongruoyan <dongruoyan@neotel.com.cn>
2018-11-14 13:11:58 +0800

    debug

--- backend/src/main/scala/com/neo/sk/carnie/http/RoomApiService.scala
+++ backend/src/main/scala/com/neo/sk/carnie/http/RoomApiService.scala
@@ -84,7 +84,7 @@ trait RoomApiService extends ServiceUtils with CirceSupport with PlayerService w
           recordInfo(record.recordId, record.roomId, record.startTime, record.endTime, userList.length, userList)
         }
 
-        complete(RecordListRsp(data.toList.sortBy(_.recordId)))
+        complete(RecordListRsp(data.toList.sortBy(_.recordId).reverse))
 
         //        recordL.map{ r =>
         //          val record = r._1
