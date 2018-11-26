4d1889a92e105a3758b8e7b17b61b4b1d981c51f
Jason <qiulinhui@neotel.com.cn>
2018-11-26 14:22:43 +0800

    debug

--- frontend/src/main/scala/com/neo/sk/carnie/paperClient/NetGameHolder.scala
+++ frontend/src/main/scala/com/neo/sk/carnie/paperClient/NetGameHolder.scala
@@ -143,7 +143,7 @@ class NetGameHolder(order: String, webSocketPara: WebSocketPara) {
             drawFunction = FrontProtocol.DrawBaseGame(gridData)
 
           case None if !firstCome =>
-            println(s"${grid.getKiller(myId).map(_._2)}")
+            println(s"killer: ${grid.getKiller(myId).map(_._2)}")
             drawFunction = FrontProtocol.DrawGameDie(grid.getKiller(myId).map(_._2))
 
           case _ =>
