19b2be219a5e5140f3685da5cd019d3871e31a47
Jason <qiulinhui@neotel.com.cn>
2018-11-28 17:11:20 +0800

    debug

--- frontend/src/main/scala/com/neo/sk/carnie/paperClient/JoinGamePage.scala
+++ frontend/src/main/scala/com/neo/sk/carnie/paperClient/JoinGamePage.scala
@@ -22,60 +22,87 @@ class JoinGamePage(order: String, webSocketPara: WebSocketPara) extends Componen
               <p style="text-align: center; margin-top: 10px;"> 选择模式</p>
             </div>
             <div>
-              <div class="gameDiv">
+              <div class="gameDiv" style="text-align: center;">
                 <div style="width: 27%; margin: 20px;">
-                  <div style="overflow: hidden;" id="1000000002">
-                    <div class="game-not-selected" style="margin-top: 20px;">
-                      <!--<img class="home-img" src="http://flowdev.neoap.com/esheep/static/img/game.png">-->
-                      <p style="text-align: center; margin-top: 10px;"> tank</p>
+                  <div style="overflow: hidden;" id="1000000012">
+                    <div class="game-not-selected" style="margin-top: 20px;height:100px;width: 100px">
+                      <img class="home-img" src="/carnie/static/img/luffy.png"></img>
+                        <p style="text-align: center; margin-top: 10px;"> tank</p>
+                      </div>
+                    </div>
+                  </div><div style="width: 27%; margin: 20px;">
+                  <div style="overflow: hidden;" id="1000000011">
+                    <div class="game-not-selected" style="margin-top: 20px;height:100px;width: 100px">
+                      <img class="home-img" src="/carnie/static/img/yang.png"></img>
+                        <p style="text-align: center; margin-top: 10px;"> medusa</p>
+                      </div>
+                    </div>
+                  </div><div style="width: 27%; margin: 20px;">
+                    <div style="overflow: hidden;" id="1000000013">
+                      <div class="game-not-selected" style="margin-top: 20px;height:100px;width: 100px" >
+                        <img class="home-img" src="/carnie/static/img/fatTiger.png"></img>
+                          <p style="text-align: center; margin-top: 10px;"> carnie</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div><div style="width: 27%; margin: 20px;">
-                <div style="overflow: hidden;" id="1000000001">
-                  <div class="game-not-selected" style="margin-top: 20px;">
-                    <!--<img class="home-img" lsrc="http://flowdev.neoap.com/esheep/static/img/game.png">-->
-                    <p style="text-align: center; margin-top: 10px;"> medusa</p>
-                  </div>
                 </div>
-              </div><div style="width: 27%; margin: 20px;">
-                <div style="overflow: hidden;" id="1000000003">
-                  <div class="game-not-selected" style="margin-top: 20px;">
-                    <!--<img class="home-img" src="http://flowdev.neoap.com/esheep/static/img/game.png">-->
-                    <p style="text-align: center; margin-top: 10px;"> carnie</p>
-                  </div>
-                </div>
-              </div><div style="width: 27%; margin: 20px;">
-                <div style="overflow: hidden;" id="1000000004">
-                  <div class="game-not-selected" style="margin-top: 20px;">
-                    <!--<img class="home-img" src="http://flowdev.neoap.com/esheep/static/img/game.png">-->
-                    <p style="text-align: center; margin-top: 10px;"> gypsy</p>
-                  </div>
-                </div>
-              </div><div style="width: 27%; margin: 20px;">
-                <div style="overflow: hidden;" id="1000000005">
-                  <div class="game-not-selected" style="margin-top: 20px;">
-                    <!--<img class="home-img" src="http://flowdev.neoap.com/esheep/static/img/game.png">-->
-                    <p style="text-align: center; margin-top: 10px;"> paradise</p>
-                  </div>
-                </div>
-              </div><div style="width: 27%; margin: 20px;">
-                <div style="overflow: hidden;" id="1000000006">
-                  <div class="game-not-selected" style="margin-top: 20px;">
-                    <!--<img class="home-img" src="http://flowdev.neoap.com/esheep/static/img/game.png">-->
-                    <p style="text-align: center; margin-top: 10px;"> thor</p>
-                  </div>
                 </div>
               </div>
-              </div>
-            </div>
-          </div>
-          <div style="overflow: hidden;" >
-            <div style="margin-top: 20px;">
-              <p style="text-align: center; margin-top: 10px;"> 选择头像</p>
-            </div>
-          </div>
-          <button class="arrow" onclick="f()">进入游戏</button>
-          </div>
+
+              <div style="overflow: hidden;" >
+                <div style="margin-top: 20px;">
+                  <p style="text-align: center; margin-top: 10px;"> 选择头像</p>
+                </div>
+                <div>
+                  <div class="gameDiv"style="text-align: center;">
+                    <div style="width: 27%; margin: 20px;">
+                      <div style="overflow: hidden;" id="1000000002">
+                        <div class="game-not-selected" style="margin-top: 20px;height:100px;width: 100px">
+                          <img class="home-img" src="/carnie/static/img/luffy.png"></img>
+                            <p style="text-align: center; margin-top: 10px;"> tank</p>
+                          </div>
+                        </div>
+                      </div><div style="width: 27%; margin: 20px;">
+                      <div style="overflow: hidden;" id="1000000001">
+                        <div class="game-not-selected" style="margin-top: 20px;height:100px;width: 100px">
+                          <img class="home-img" src="/carnie/static/img/yang.png" ></img>
+                            <p style="text-align: center; margin-top: 10px;"> medusa</p>
+                          </div>
+                        </div>
+                      </div><div style="width: 27%; margin: 20px;">
+                        <div style="overflow: hidden;" id="1000000003">
+                          <div class="game-not-selected" style="margin-top: 20px;height:100px;width: 100px">
+                            <img class="home-img" src="/carnie/static/img/fatTiger.png" ></img>
+                              <p style="text-align: center; margin-top: 10px;"> carnie</p>
+                            </div>
+                          </div>
+                        </div><div style="width: 27%; margin: 20px;">
+                          <div style="overflow: hidden;" id="1000000004">
+                            <div class="game-not-selected" style="margin-top: 20px;height:100px;width: 100px">
+                              <img class="home-img" src="/carnie/static/img/smile.png" ></img>
+                                <p style="text-align: center; margin-top: 10px;"> gypsy</p>
+                              </div>
+                            </div>
+                          </div><div style="width: 27%; margin: 20px;">
+                            <div style="overflow: hidden;" id="1000000005">
+                              <div class="game-not-selected" style="margin-top: 20px;height:100px;width: 100px">
+                                <img class="home-img" src="/carnie/static/img/pig.png" ></img>
+                                  <p style="text-align: center; margin-top: 10px;"> paradise</p>
+                                </div>
+                              </div>
+                            </div><div style="width: 27%; margin: 20px;">
+                              <div style="overflow: hidden;" id="1000000006">
+                                <div class="game-not-selected" style="margin-top: 20px;height:100px;width: 100px">
+                                  <img class="home-img" src="/carnie/static/img/Bob.png"></img>
+                                    <p style="text-align: center; margin-top: 10px;"> thor</p>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      <button class="arrow" onclick="f()">进入游戏</button>
       </body>
     </html>
   }
