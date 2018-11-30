3b3779d6b3e8a411be9598641230cd4aee495116
Jason <qiulinhui@neotel.com.cn>
2018-11-30 13:01:33 +0800

    debug

--- frontend/src/main/scala/com/neo/sk/carnie/Main.scala
+++ frontend/src/main/scala/com/neo/sk/carnie/Main.scala
@@ -23,42 +23,42 @@ object Main extends js.JSApp {
   }
 
   def selectPage(): Elem = {
-    new JoinGamePage("playGame", PlayGamePara("test", "test", mode = 1)).render
-//    val url = dom.window.location.href.split("carnie/")(1)
-//    val info = url.split("\\?")
-//    val playerMsgMap = info(1).split("&").map {
-//      a =>
-//        val b = a.split("=")
-//        (b(0), b(1))
-//    }.toMap
-//    val sendData = PlayerMsg(playerMsgMap).asJson.noSpaces
-//    println(s"sendData: $sendData")
-//    info(0) match {
-//      case "playGame" =>
-//        val playerId = if (playerMsgMap.contains("playerId")) playerMsgMap("playerId") else "unKnown"
-//        val playerName = if (playerMsgMap.contains("playerName")) playerMsgMap("playerName") else "unKnown"
-//        //        val img = 0
-////        new NetGameHolder("playGame", PlayGamePara(playerId, playerName)).render
-//        new JoinGamePage("playGame", PlayGamePara(playerId, playerName, mode = 1)).render
-//
-//      case "watchGame" =>
-//        val roomId = playerMsgMap.getOrElse("roomId", "1000")
-//        val playerId = playerMsgMap.getOrElse("playerId", "unknown")
-//        val accessCode = playerMsgMap.getOrElse("accessCode", "test123")
-//        println(s"Frontend-roomId: $roomId, playerId:$playerId, accessCode: $accessCode")
-//        new NetGameHolder("watchGame", WatchGamePara(roomId, playerId, accessCode)).render
-//
-//      case "watchRecord" =>
-//        val recordId = playerMsgMap.getOrElse("recordId", "1000001")
-//        val playerId = playerMsgMap.getOrElse("playerId", "1000001")
-//        val frame = playerMsgMap.getOrElse("frame", "0")
-//        val accessCode = playerMsgMap.getOrElse("accessCode", "abc")
-//        new NetGameHolder4WatchRecord(WatchRecordPara(recordId, playerId, frame, accessCode)).render
-//
-//      case _ =>
-//        println("Unknown order!")
-//        <div>Error Page</div>
-//    }
+//    new JoinGamePage("playGame", PlayGamePara("test", "test", mode = 1)).render
+    val url = dom.window.location.href.split("carnie/")(1)
+    val info = url.split("\\?")
+    val playerMsgMap = info(1).split("&").map {
+      a =>
+        val b = a.split("=")
+        (b(0), b(1))
+    }.toMap
+    val sendData = PlayerMsg(playerMsgMap).asJson.noSpaces
+    println(s"sendData: $sendData")
+    info(0) match {
+      case "playGame" =>
+        val playerId = if (playerMsgMap.contains("playerId")) playerMsgMap("playerId") else "unKnown"
+        val playerName = if (playerMsgMap.contains("playerName")) playerMsgMap("playerName") else "unKnown"
+        //        val img = 0
+//        new NetGameHolder("playGame", PlayGamePara(playerId, playerName)).render
+        new JoinGamePage("playGame", PlayGamePara(playerId, playerName, mode = 1)).render
+
+      case "watchGame" =>
+        val roomId = playerMsgMap.getOrElse("roomId", "1000")
+        val playerId = playerMsgMap.getOrElse("playerId", "unknown")
+        val accessCode = playerMsgMap.getOrElse("accessCode", "test123")
+        println(s"Frontend-roomId: $roomId, playerId:$playerId, accessCode: $accessCode")
+        new NetGameHolder("watchGame", WatchGamePara(roomId, playerId, accessCode)).render
+
+      case "watchRecord" =>
+        val recordId = playerMsgMap.getOrElse("recordId", "1000001")
+        val playerId = playerMsgMap.getOrElse("playerId", "1000001")
+        val frame = playerMsgMap.getOrElse("frame", "0")
+        val accessCode = playerMsgMap.getOrElse("accessCode", "abc")
+        new NetGameHolder4WatchRecord(WatchRecordPara(recordId, playerId, frame, accessCode)).render
+
+      case _ =>
+        println("Unknown order!")
+        <div>Error Page</div>
+    }
   }
 
   def show(): Cancelable = {
