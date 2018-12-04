cb624c4dcc6aed8e47d4839526751b45d55bf6c5
dongruoyan <dongruoyan@neotel.com.cn>
2018-12-04 13:57:37 +0800

    debug

--- client/src/main/scala/com/neo/sk/carnie/common/AppSetting.scala
+++ client/src/main/scala/com/neo/sk/carnie/common/AppSetting.scala
@@ -1,30 +1,14 @@
 package com.neo.sk.carnie.common
 
+import java.io.File
 import com.typesafe.config.ConfigFactory
-import org.slf4j.LoggerFactory
 
 /**
   * Created by dry on 2018/10/23.
   **/
 object AppSetting {
 
-  val log = LoggerFactory.getLogger(this.getClass)
   val config = ConfigFactory.parseResources("product.conf").withFallback(ConfigFactory.load())
-
-  val appConfig = config.getConfig("app")
-  val gameDataDirectoryPath = appConfig.getString("gameDataDirectoryPath")
-
-  val httpInterface = appConfig.getString("http.interface")
-  val httpPort = appConfig.getInt("http.port")
-
-
-  val appSecureMap = {
-    import collection.JavaConverters._
-    val appIds = appConfig.getStringList("client.appIds").asScala
-    val secureKeys = appConfig.getStringList("client.secureKeys").asScala
-    require(appIds.length == secureKeys.length, "appIdList.length and secureKeys.length not equel.")
-    appIds.zip(secureKeys).toMap
-  }
 
   val esheepConfig = config.getConfig("dependence.esheep")
   val esheepProtocol = esheepConfig.getString("protocol")
@@ -35,13 +19,17 @@ object AppSetting {
   val esheepGameId = esheepConfig.getLong("gameId")
   val esheepGsKey = esheepConfig.getString("gsKey")
 
-  val slickConfig = config.getConfig("slick.db")
-  val slickUrl = slickConfig.getString("url")
-  val slickUser = slickConfig.getString("user")
-  val slickPassword = slickConfig.getString("password")
-  val slickMaximumPoolSize = slickConfig.getInt("maximumPoolSize")
-  val slickConnectTimeout = slickConfig.getInt("connectTimeout")
-  val slickIdleTimeout = slickConfig.getInt("idleTimeout")
-  val slickMaxLifetime = slickConfig.getInt("maxLifetime")
+  val file = new File("bot.conf")
+  if (file.isFile && file.exists) {
+    val botConfig = ConfigFactory.parseResources("bot.conf").withFallback(ConfigFactory.load())
+
+    val appConfig = botConfig.getConfig("app")
+
+    val userConfig = appConfig.getConfig("user")
+    val email = userConfig.getString("email")
+    val psw = userConfig.getString("psw")
+
+    val render = appConfig.getBoolean("render")
+  }
 
 }
