d0214fb80c6b0e6fd4fc49f3bfd2c8319401ea67
Jason <qiulinhui@neotel.com.cn>
2018-12-04 15:15:38 +0800

    debug

--- frontend/src/main/scala/com/neo/sk/carnie/paperClient/NetGameHolder4WatchGame.scala
+++ frontend/src/main/scala/com/neo/sk/carnie/paperClient/NetGameHolder4WatchGame.scala
@@ -61,10 +61,10 @@ class NetGameHolder4WatchGame(order: String, webSocketPara: WebSocketPara) exten
   private[this] val bgm3 = dom.document.getElementById("bgm3").asInstanceOf[HTMLAudioElement]
   private[this] val bgm4 = dom.document.getElementById("bgm4").asInstanceOf[HTMLAudioElement]
   private[this] val bgm5 = dom.document.getElementById("bgm5").asInstanceOf[HTMLAudioElement]
-  private[this] val bgm6 = dom.document.getElementById("bgm6").asInstanceOf[HTMLAudioElement]
   private[this] val bgm7 = dom.document.getElementById("bgm7").asInstanceOf[HTMLAudioElement]
   private[this] val bgm8 = dom.document.getElementById("bgm8").asInstanceOf[HTMLAudioElement]
-  private[this] val bgmList = List(bgm1, bgm2, bgm3, bgm4, bgm5, bgm6, bgm7, bgm8)
+  private[this] val bgmList = List(bgm1, bgm2, bgm3, bgm4, bgm5, bgm7, bgm8)
+  private val bgmAmount = bgmList.length
   private var BGM = dom.document.getElementById("bgm4").asInstanceOf[HTMLAudioElement]
   private[this] val rankCanvas = dom.document.getElementById("RankView").asInstanceOf[Canvas] //把排行榜的canvas置于最上层，所以监听最上层的canvas
 
@@ -95,7 +95,7 @@ class NetGameHolder4WatchGame(order: String, webSocketPara: WebSocketPara) exten
 
   def startGame(): Unit = {
     drawGame.drawGameOn()
-    BGM = bgmList(getRandom(8))
+    BGM = bgmList(getRandom(bgmAmount))
     BGM.play()
     dom.window.setInterval(() => gameLoop(), frameRate)
     dom.window.setInterval(() => {
@@ -163,7 +163,7 @@ class NetGameHolder4WatchGame(order: String, webSocketPara: WebSocketPara) exten
           case Some(_) =>
             if (firstCome) firstCome = false
             if (BGM.paused) {
-              BGM = bgmList(getRandom(8))
+              BGM = bgmList(getRandom(bgmAmount))
               BGM.play()
             }
             FrontProtocol.DrawBaseGame(gridData)
