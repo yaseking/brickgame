081b9950dc287a37463876ad2f2dd61ffa7e2665
litianyu <87394376@qq.com>
2018-12-12 15:29:24 +0800

    botListScene

--- client/src/main/scala/com/neo/sk/carnie/scene/BotListScene.scala
+++ client/src/main/scala/com/neo/sk/carnie/scene/BotListScene.scala
@@ -1,19 +1,22 @@
 package com.neo.sk.carnie.scene
 
-import javafx.collections.{FXCollections, ObservableList, ObservableMap}
+import javafx.collections.{FXCollections, ObservableList}
 import javafx.geometry.Pos
+import javafx.scene.canvas.Canvas
+import javafx.scene.{Group, Scene}
 import javafx.scene.control._
 import javafx.scene.image.{Image, ImageView}
-import javafx.scene.{Group, Scene}
-import javafx.scene.layout.{BorderPane, HBox, Priority}
+import javafx.scene.layout.{BorderPane, HBox}
+import javafx.scene.paint.Color
+import javafx.scene.text.Font
 
 import scala.collection.mutable
 
-abstract class RoomListSceneListener{
-  def confirm(roomId: Int, mode: Int, hasPwd: Boolean)
+abstract class BotListSceneListener {
+  def joinGame(mode: Int, img: Int)
 }
 
-class RoomListScene {
+class BotListScene {
   val width = 500
   val height = 500
 
@@ -24,17 +27,17 @@ class RoomListScene {
   val hBox = new HBox(20)
 
   val roomLockMap:mutable.HashMap[Int, (Int, Boolean)] = mutable.HashMap.empty[Int, (Int, Boolean)]//(roomId -> (mode, hasPwd))
-  var botList:List[String] = List.empty[String]
+  var roomList:List[String] = List.empty[String]
   private val observableList:ObservableList[String] = FXCollections.observableArrayList()
   private val listView = new ListView[String](observableList)
-  private val confirmBtn = new Button("进入房间")
+  private val confirmBtn = new Button("加入游戏")
 
   var listener: RoomListSceneListener = _
 
-//  confirmBtn.setPrefSize(100,20)
+  //  confirmBtn.setPrefSize(100,20)
   hBox.getChildren.addAll(listView, confirmBtn)
   hBox.setAlignment(Pos.CENTER)
-//  HBox.setHgrow(listView,Priority.ALWAYS)
+  //  HBox.setHgrow(listView,Priority.ALWAYS)
   //  borderPane.prefHeightProperty().bind(scene.heightProperty())
   borderPane.prefWidthProperty().bind(scene.widthProperty())
   borderPane.setCenter(hBox)
@@ -42,7 +45,7 @@ class RoomListScene {
 
   def updateRoomList(roomList:List[String]):Unit = {
     println(s"updateRoomList: $roomList")
-    this.botList = roomList
+    this.roomList = roomList
     this.roomLockMap.clear()
     observableList.clear()
     roomList.sortBy(t => t).map { s =>
@@ -53,11 +56,11 @@ class RoomListScene {
       roomLockMap += roomId -> (roomInfos(1).toInt, hasPwd)
       roomId.toString + "-" + modeName
     } foreach observableList.add
-//    println(observableList)
+    //    println(observableList)
   }
 
   listView.setCellFactory(_ => new ListCell[String](){//todo 找锁的图片和无锁的图片，登录图片
-    val img = new ImageView("img/Bob.png")
+  val img = new ImageView("img/Bob.png")
     val img1 = new ImageView("img/luffy.png")
     img.setFitWidth(15)
     img.setFitHeight(15)
@@ -70,7 +73,7 @@ class RoomListScene {
         setText(null)
         setGraphic(null)
       } else {
-//        println(s"item: $item")
+        //        println(s"item: $item")
         val roomId = item.split("-")(0).toInt
         if(roomLockMap.contains(roomId) && roomLockMap(roomId)._2){
           setGraphic(img)
@@ -90,5 +93,5 @@ class RoomListScene {
     val roomId = selectedInfo.split("-").head.toInt
     listener.confirm(roomId, roomLockMap(roomId)._1, roomLockMap(roomId)._2)
   }
-//  confirmBtn.setOnAction(_ => println(listView.getSelectionModel.selectedItemProperty().get()))(Int, Int, Boolean)
+  //  confirmBtn.setOnAction(_ => println(listView.getSelectionModel.selectedItemProperty().get()))(Int, Int, Boolean)
 }
