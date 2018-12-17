c160534f9f5146e4b23ba8c132c617849b574953
dongruoyan <dongruoyan@neotel.com.cn>
2018-12-17 16:24:38 +0800

    debug

--- backend/src/main/scala/com/neo/sk/carnie/core/BotManager.scala
+++ backend/src/main/scala/com/neo/sk/carnie/core/BotManager.scala
@@ -3,13 +3,17 @@ package com.neo.sk.carnie.core
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors, StashBuffer, TimerScheduler}
 import akka.actor.typed.ActorRef
 import akka.actor.typed.Behavior
+import org.slf4j.LoggerFactory
 
 /**
   * Created by dry on 2018/12/17.
   **/
 object BotManager {
+  private val log = LoggerFactory.getLogger(this.getClass)
 
   sealed trait Command
+
+//  case class InitBot(roomId) extends Command
 
   val behaviors: Behavior[Command] = init()
 
@@ -27,7 +31,7 @@ object BotManager {
       msg match {
 
         case unknownMsg@_ =>
-//          log.warn(s"${ctx.self.path} unknown msg: $unknownMsg")
+          log.warn(s"${ctx.self.path} unknown msg: $unknownMsg")
           stashBuffer.stash(unknownMsg)
           Behaviors.unhandled
       }
