fa9db052a9a082b6c47b0592fc1aa47b27e12754
pro <haoshuhan@neotel.com.cn>
2018-12-19 11:53:29 +0800

    .

--- frontend/src/main/scala/com/neo/sk/carnie/paperClient/NetGameHolder.scala
+++ frontend/src/main/scala/com/neo/sk/carnie/paperClient/NetGameHolder.scala
@@ -393,6 +393,8 @@ class NetGameHolder(order: String, webSocketPara: WebSocketPara, mode: Int, img:
       case data: Protocol.NewSnakeInfo =>
         println(s"!!!!!!new snake---${data.snake} join!!!isContinue$isContinue")
         newSnakeInfo = Some(data)
+        if(data.snake.map(_.id).contains(myId)) isContinue = true
+        dom.window.requestAnimationFrame(gameRender())
 
       case Protocol.SomeOneKilled(killedId, killedName, killerName) =>
         killInfo = Some(killedId, killedName, killerName)
