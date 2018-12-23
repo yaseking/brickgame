7ff726f9e55b19b916c36f31758d5ba574364fab
Jason <qiulinhui@neotel.com.cn>
2018-12-23 21:01:17 +0800

    debug

--- frontend/src/main/scala/com/neo/sk/carnie/paperClient/DrawGame.scala
+++ frontend/src/main/scala/com/neo/sk/carnie/paperClient/DrawGame.scala
@@ -383,7 +383,7 @@ class DrawGame(
       field.scanField.foreach { fids =>
         fids.y.foreach{y =>
           fids.x.foreach{x =>
-            ctx.fillRect((x._1 + offx) * canvasUnit, (y._1 + offy) * canvasUnit, canvasUnit * (x._2 - x._1 + 1), canvasUnit * (y._2 - y._1 + 1) * 1.05)
+            ctx.fillRect((x._1 + offx) * canvasUnit, (y._1 + offy) * canvasUnit, canvasUnit * (x._2 - x._1 + 1), canvasUnit * (y._2 - y._1 + 1.04))
           }
         }
       }
