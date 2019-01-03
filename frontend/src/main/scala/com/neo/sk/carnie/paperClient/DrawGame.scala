326960b52b34f4b93ca37bb693453fe83072a3b1
Jason <qiulinhui@neotel.com.cn>
2019-01-03 14:13:10 +0800

    debug

--- frontend/src/main/scala/com/neo/sk/carnie/paperClient/DrawGame.scala
+++ frontend/src/main/scala/com/neo/sk/carnie/paperClient/DrawGame.scala
@@ -378,7 +378,18 @@ class DrawGame(
 
     ctx.globalAlpha = offsetTime.toFloat / frameRate * 1
     if(newFieldInfo.nonEmpty){
-      newFieldInfo.get.fieldDetails.foreach { field => //按行渲染
+      var newFieldInWindow: List[FieldByColumn] = Nil
+      newFieldInfo.get.fieldDetails.foreach { user =>
+        if (snakes.exists(_.id == user.uid)) {
+          newFieldInWindow = FieldByColumn(user.uid, user.scanField.map { field =>
+            ScanByColumn(field.y.filter(y => y._1 < maxPoint.y || y._2 > minPoint.y),
+              field.x.filter(x => x._1 < maxPoint.x || x._2 > minPoint.x))
+          }) :: newFieldInWindow
+        }
+      }
+
+      println(s"newField: ${newFieldInfo.get}")
+      newFieldInWindow.foreach { field => //按行渲染
         val color = snakes.find(_.id == field.uid).map(_.color).getOrElse(ColorsSetting.defaultColor)
         ctx.fillStyle = color
         field.scanField.foreach { fids =>
@@ -396,6 +407,7 @@ class DrawGame(
     fieldInWindow.foreach { field => //按行渲染
       val color = snakes.find(_.id == field.uid).map(_.color).getOrElse(ColorsSetting.defaultColor)
       ctx.fillStyle = color
+
       //      field.scanField.foreach { point =>
       //        point.x.foreach { x =>
       //          ctx.fillRect((x._1 + offx) * canvasUnit, (point.y + offy) * canvasUnit, canvasUnit * (x._2 - x._1 + 1), canvasUnit * 1.05)
