30432e011ca16c15d26a6646ed4bea83e74a5a34
Jason <qiulinhui@neotel.com.cn>
2019-01-03 20:10:25 +0800

    debug

--- frontend/src/main/scala/com/neo/sk/carnie/paperClient/NetGameHolder4WatchGame.scala
+++ frontend/src/main/scala/com/neo/sk/carnie/paperClient/NetGameHolder4WatchGame.scala
@@ -305,7 +305,7 @@ class NetGameHolder4WatchGame(order: String, webSocketPara: WebSocketPara) exten
           val id = grid.carnieMap(carnieId)
           if (id == myId) { //收到自己的进行校验是否与预判一致，若不一致则回溯
             //            println(s"recv:$r")
-//            println(s"rev my action!!! $r")
+            println(s"rev my action!!! $r")
             if (myActionHistory.get(actionId).isEmpty) { //前端没有该项，则加入
               grid.addActionWithFrame(id, keyCode, frame)
               if (frame < grid.frameCount) {
