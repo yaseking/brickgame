799e8809f971cc0d02bdfb4a8d70452d94296e88
pro <haoshuhan@neotel.com.cn>
2019-01-19 11:27:54 +0800

    。

--- frontend/src/main/scala/org/seekloud/carnie/paperClient/DrawGame.scala
+++ frontend/src/main/scala/org/seekloud/carnie/paperClient/DrawGame.scala
@@ -573,7 +573,7 @@ class DrawGame(
     val currentRankBaseLine = 2
     var index = 0
     rankCtx.font = "10px Helvetica"
-    drawTextLine("Version:20190118", rightBegin.toInt+100, index, currentRankBaseLine-1)
+    drawTextLine("Version:20190119", rightBegin.toInt+100, index, currentRankBaseLine-1)
     rankCtx.font = "14px Helvetica"
     drawTextLine(s" --- Current Rank ---   players:$currentNum", rightBegin.toInt, index, currentRankBaseLine)
     if (currentRank.lengthCompare(3) >= 0) {
