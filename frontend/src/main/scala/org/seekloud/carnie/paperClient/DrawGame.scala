ac245484b234c9418ad5efaf99e48ca88bf537d3
Jason <qiulinhui@neotel.com.cn>
2019-01-21 15:59:44 +0800

    VISION

--- frontend/src/main/scala/org/seekloud/carnie/paperClient/DrawGame.scala
+++ frontend/src/main/scala/org/seekloud/carnie/paperClient/DrawGame.scala
@@ -395,8 +395,20 @@ class DrawGame(
     scale = Math.max(1 - grid.getMyFieldCount(uid, fieldByXInWindow.filter(_.uid==uid).flatMap(_.scanField)) * 0.00002, 0.94)
     ctx.save()
     setScale(scale, windowBoundary.x / 2, windowBoundary.y / 2)
-    ctx.fillStyle = "#B4DCFF"
-    ctx.fillRect(0,0,windowBoundary.x,windowBoundary.y)
+    ctx.globalAlpha = 0.5
+    val w = windowBoundary.x / 2
+    val h = windowBoundary.y / 2
+    val radialGradient1 = ctx.createRadialGradient(w, h, 200, w, h,1000)
+
+    radialGradient1.addColorStop(0, "#FFFFFF")
+
+    radialGradient1.addColorStop(1, "#B4DCFF")
+
+    ctx.fillStyle = radialGradient1
+
+    ctx.fillRect(10, 10, 200, 200)
+//    ctx.fillStyle = "#B4DCFF"
+//    ctx.fillRect(0,0,windowBoundary.x,windowBoundary.y)
 
     ctx.globalAlpha = 0.6
     bodyInWindow.foreach { bds =>
