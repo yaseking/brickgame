fa2d3b264a92b76a1c184922ce9fa7616f01713c
Jason <qiulinhui@neotel.com.cn>
2019-01-21 15:14:34 +0800

    VISION

--- frontend/src/main/scala/org/seekloud/carnie/paperClient/DrawGame.scala
+++ frontend/src/main/scala/org/seekloud/carnie/paperClient/DrawGame.scala
@@ -395,7 +395,7 @@ class DrawGame(
     scale = Math.max(1 - grid.getMyFieldCount(uid, fieldByXInWindow.filter(_.uid==uid).flatMap(_.scanField)) * 0.00002, 0.94)
     ctx.save()
     setScale(scale, windowBoundary.x / 2, windowBoundary.y / 2)
-    ctx.fillStyle = "#87CEFA"
+    ctx.fillStyle = "#B4DCFF"
     ctx.fillRect(0,0,windowBoundary.x,windowBoundary.y)
 
     ctx.globalAlpha = 0.6
